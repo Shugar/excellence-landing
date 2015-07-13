1ce89ff43716f218dbf3349d864ee2c0e5c7c6d7
Shugar <shugar348@gmail.com>
2015-07-13 07:16:43 +0300

    map pin work now

--- app/main.js
+++ app/main.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
     
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var icon1 = "../images/Geo 1.svg";
-    var icon2 = "../images/Geo 2.svg";
+    var icon1 = "images/geo.svg";
+    var icon2 = "images/geohover.svg";
 
     
 
@@ -44,57 +44,6 @@ $(document).ready(function(){
   google.maps.event.addDomListener(window, 'load', initialize);
 
 });
-
-
-
-$(document).ready(function(){
-  $('select').each(function(){
-      var $this = $(this), numberOfOptions = $(this).children('option').length;
-    
-      $this.addClass('select-hidden'); 
-      $this.wrap('<div class="select"></div>');
-      $this.after('<div class="select-styled"></div>');
-
-      var $styledSelect = $this.next('div.select-styled');
-      $styledSelect.text($this.children('option').eq(0).text());
-    
-      var $list = $('<ul />', {
-          'class': 'select-options'
-      }).insertAfter($styledSelect);
-    
-      for (var i = 0; i < numberOfOptions; i++) {
-          $('<li />', {
-              text: $this.children('option').eq(i).text(),
-              rel: $this.children('option').eq(i).val()
-          }).appendTo($list);
-      }
-    
-      var $listItems = $list.children('li');
-    
-      $styledSelect.click(function(e) {
-          e.stopPropagation();
-          $('div.select-styled.active').each(function(){
-              $(this).removeClass('active').next('ul.select-options').hide();
-          });
-          $(this).toggleClass('active').next('ul.select-options').toggle();
-      });
-    
-      $listItems.click(function(e) {
-          e.stopPropagation();
-          $styledSelect.text($(this).text()).removeClass('active');
-          $this.val($(this).attr('rel'));
-          $list.hide();
-          //console.log($this.val());
-      });
-    
-      $(document).click(function() {
-          $styledSelect.removeClass('active');
-          $list.hide();
-      });
-
-  });
-});
-
 
 
 
